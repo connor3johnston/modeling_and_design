1ba8f47712de8a6e80d8235dbf6650f13f42091b
Connor Johnston <connor3johnston@gmail.com>
2019-08-27 11:44:09 -0500

    updated FinderTest

--- assignment_2/src/test/java/FinderTest.java
+++ assignment_2/src/test/java/FinderTest.java
@@ -1,4 +1,5 @@
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 /** Test for Finder. */
@@ -6,51 +7,51 @@ public class FinderTest {
 
     @Test
     public void findMax_validArrayOfLengthOne() {
-        Assert.assertEquals(Finder.findMax(new int[]{1}), new Integer(1));
+        assertEquals(Finder.findMax(new int[]{1}), new Integer(1));
     }
 
     @Test
     public void findMax_validUnorderedArrayOfLengthGreaterThanOne() {
-        Assert.assertEquals(Finder.findMax(new int[]{4, 5, 1, 2, 3}), new Integer(5));
+        assertEquals(Finder.findMax(new int[]{4, 5, 1, 2, 3}), new Integer(5));
     }
 
     @Test
     public void findMax_validOrderedArrayOfLengthGreaterThanOne() {
-        Assert.assertEquals(Finder.findMax(new int[]{1,2,3}), new Integer(3));
+        assertEquals(Finder.findMax(new int[]{1,2,3}), new Integer(3));
     }
 
     @Test
     public void findMax_invalidEmptyArray() {
-        Assert.assertEquals(Finder.findMax(new int[]{}), null);
+        assertEquals(Finder.findMax(new int[]{}), null);
     }
 
     @Test
     public void findMax_invalidNullArray() {
-        Assert.assertEquals(Finder.findMax(null), null);
+        assertEquals(Finder.findMax(null), null);
     }
 
     @Test
     public void findMin_validArrayOfLengthOne() {
-        Assert.assertEquals(Finder.findMin(new int[]{1}), new Integer(1));
+        assertEquals(Finder.findMin(new int[]{1}), new Integer(1));
     }
 
     @Test
     public void findMin_validUnorderedArrayOfLengthGreaterThanOne() {
-        Assert.assertEquals(Finder.findMin(new int[]{4, 5, 1, 2, 3}), new Integer(1));
+        assertEquals(Finder.findMin(new int[]{4, 5, 1, 2, 3}), new Integer(1));
     }
 
     @Test
     public void findMin_validOrederedArrayOfLengthGreaterThanOne() {
-        Assert.assertEquals(Finder.findMin(new int[]{1,2,3}), new Integer(1));
+        assertEquals(Finder.findMin(new int[]{1,2,3}), new Integer(1));
     }
 
     @Test
     public void findMin_invalidEmptyArray() {
-        Assert.assertEquals(Finder.findMin(new int[]{}), null);
+        assertEquals(Finder.findMin(new int[]{}), null);
     }
 
     @Test
     public void findMin_invalidNullArray() {
-        Assert.assertEquals(Finder.findMin(null), null);
+        assertEquals(Finder.findMin(null), null);
     }
 }
